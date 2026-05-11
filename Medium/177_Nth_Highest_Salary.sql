642b99dfc979f9238d21a3c38acf4232758d32c8
Dev Prajapati <prajapatidev200@email.com>
2026-05-11 12:39:58 +0530

    177 Solution

---
+++ Medium/177_Nth_Highest_Salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+    SELECT SALARY
+    FROM (
+        SELECT SALARY,
+        DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RNK
+        FROM EMPLOYEE
+    ) T
+    WHERE RNK = N
+    LIMIT 1
+  );
+END
